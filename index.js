8a950f3a325896f3b3ce0670d31b2825402995a3
Diwas Atreya <diwasatreya45@gmail.com>
2023-06-17 14:05:07 +0545

    test
    Hm

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ client.on("ready", () => {
 
 
 
-//  For Watching Status
+//  For Watching Statuss
 // client.on("ready", () => {
 // client.user.setActivity(`Chilling with owner`, { type:         "STREAMING",
 // url: "https://www.twitch.tv/nocopyrightsounds"})
