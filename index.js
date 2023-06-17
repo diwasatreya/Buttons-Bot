072b74a2f6ae1523143128e7e5aed0f9d206f71e
Diwas Atreya <diwasatreya45@gmail.com>
2023-06-17 14:06:56 +0545

    again testing
    1
    
    Co-Authored-By: Sushant Subedi <96135912+sushantsubedii@users.noreply.github.com>

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ message.channel.send(`${math.evaluate(`${message.content} + 1`)}`)
 });
 
 
-// Do not change anything here
+// Do not change anything heree
 require('http').createServer((req, res) => res.end(`
  |-----------------------------------------|
  |              Informations               |
